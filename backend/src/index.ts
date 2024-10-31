c5fcf547532ec1e749603398c99fd7fca591eef7
sharabh mishra <sharabhmishra15@gmail.com>
2024-10-31 08:32:07 +0530

    cors added to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import { withAccelerate } from "@prisma/extension-accelerate";
 import { decode, sign, verify } from "hono/jwt";
 import userRouter from "./routes/user"
 import blogRouter from "./routes/blog"
+import { cors } from "hono/cors";
 
 const app = new Hono<{
   Bindings: {
@@ -15,6 +16,8 @@ const app = new Hono<{
     prisma: string;
   };
 }>();
+
+app.use('/*', cors())
 
 // app.use("*", async (c, next) => {
 //   const prisma = new PrismaClient({
